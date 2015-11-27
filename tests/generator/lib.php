781d065cd0c5f4b2a558e57aea8f35fe0c7c1ebe
Mike Churchward <mike@remote-learner.net>
2015-11-27 09:09:23 -0500

    CONTRIB-5900 - Initial check-in to start unit test creation.

---
+++ tests/generator/lib.php
@@ -0,0 +1,72 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * mod_questionnaire data generator
+ *
+ * @package    mod_questionnaire
+ * @copyright  2015 Mike Churchward (mike@churchward.ca)
+ * @author     Mike Churchward
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+
+class mod_questionnaire_generator extends testing_module_generator {
+
+    public function create_instance($record = null, array $options = null) {
+        global $CFG;
+
+        $record = (object)(array)$record;
+
+        $defaultquestionnairesettings = array(
+            'qtype'               	=> 0,
+            'respondenttype'		=> 'fullname',
+            'resp_eligible'			=> 'all',
+            'resp_view'				=> 0,
+            'opendate'				=> 0,
+            'closedate'				=> 0,
+            'resume'				=> 0,
+            'navigate'				=> 0,
+            'grade'					=> 0,
+            'sid'					=> 0,
+            'timemodified'			=> time(),
+            'completionsubmit'		=> 0,
+            'autonum'				=> 3,
+            'create'				=> 'new-0',		// used in form only to indicate a new, empty instance.
+        );
+
+        foreach ($defaultquestionnairesettings as $name => $value) {
+            if (!isset($record->{$name})) {
+                $record->{$name} = $value;
+            }
+        }
+
+        return parent::create_instance($record, (array)$options);
+    }
+
+    public function create_content($questionnaire, $record = array()) {
+        global $DB, $CFG;
+        require_once($CFG->dirroot.'/mod/questionnaire/locallib.php');
+
+        $survey = $DB->get_record('questionnaire_survey', array('id' => $questionnaire->sid), '*', MUST_EXIST);
+        foreach ($record as $name => $value) {
+        	$survey->{$name} = $value;
+        }
+        return $questionnaire->survey_update($survey);
+    }
+}
